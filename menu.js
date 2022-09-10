c0a3f7a8128f3f59692e06482815f714e5476d39
radovanbotik <radovanbotik@gmail.com>
2022-09-10 16:25:47 +0200

    created some menu data for the restaurant menu project

---
+++ menu.js
@@ -0,0 +1,16 @@
+const entiremenu = [
+  {
+    pizza: {
+      "32cm": {
+        margherita: {
+          id: 1,
+          ingredients: ["tomato puree", "tomato", "oregano", "mozzarella"],
+          price: 600,
+          size: "32cm",
+          weight: "500g",
+          allergens: [1, 3, 7],
+        },
+      },
+    },
+  },
+];
